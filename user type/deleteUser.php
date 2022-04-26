2d16bb9c9f7d97112682ea66d6f83c4e15e7076b
youssefsharabas1 <101363742+youssefsharabas1@users.noreply.github.com>
2022-04-27 00:02:40 +0200

    Delete user type directory

--- user type/deleteUser.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "Userfnc.php";
-include_once "Functins.php";
-//DeleteRecord("user type.txt", $_REQUEST["id"]) 
-$id=$_REQUEST["id"];
-DeleteUser($id);
-header("Location: ListAllUsers.php?Msg=Record $id was Deleted sucessfully");
-?>
--- user type/update.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "Userfnc.php";
-$id=$_REQUEST["id"];
-$Name=$_REQUEST["name"];
-UpdateUser($id,$Name);
-header("Location: ListAllUsers.php?msg=done");
-?>
