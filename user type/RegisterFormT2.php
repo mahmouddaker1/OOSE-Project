016b4c9cc4e24804b1d69c35ab7e1ee8c84e661b
youssefsharabas1 <101363742+youssefsharabas1@users.noreply.github.com>
2022-04-27 00:11:46 +0200

    Add files via upload

---
+++ user type/RegisterFormT2.php
@@ -0,0 +1,19 @@
+<?php
+include "UserFncT.php";
+include_once "FunctinsT.php";
+$myfile = fopen("user type.txt","a+");
+$id=getLastId("user type.txt", "~")+1;
+fwrite($myfile,$id."~".$_POST["Name"]."\r\n");
+fclose($myfile);
+	header("Location: ListAllUsersType.php?Msg=Record Added Sucessfully");
+/*$pass=Encrypt($_REQUEST["Password"],2);
+
+if (addUser($_POST["Email"],$pass,$_REQUEST["FullName"],$_REQUEST["DOB"]))
+{
+	echo "Success";
+}
+else
+{
+	echo "Duplicate ID";
+}*/
+?>
